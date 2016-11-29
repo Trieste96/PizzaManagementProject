ead02b7298fe221c92b44d40f308d056d3204402
Trieste96 <truongminhtriet96@gmail.com>
2016-11-30 01:16:05 +0700

    Hoàn thiện phần đăng nhập

---
+++ BUS/AuthenticationBUS.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+using System.Security.Cryptography;
+
+namespace BUS
+{
+    public class AuthenticationBUS
+    {
+        private string GetMd5Hash(MD5 md5, string input)
+        {
+            byte[] result = md5.ComputeHash(Encoding.UTF8.GetBytes(input));
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                sb.Append(result[i].ToString("x2"));
+            }
+            return sb.ToString();
+        }
+        public void verifyAccount(NhanVien nv)
+        {
+            //Mã hoá mật khẩu thành mã băm MD5
+            MD5 md5 = MD5.Create();
+            nv.MatKhau = GetMd5Hash(md5, nv.MatKhau);
+
+            AuthenticationDAO dao = new AuthenticationDAO();
+            dao.verifyAccount(nv);
+        }
+    }
+}
--- PizzaManagement/AuthenticationUI.cs
+++ PizzaManagement/AuthenticationUI.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DTO;
 
 namespace PizzaManagement
 {
@@ -27,7 +28,26 @@ namespace PizzaManagement
 
         private void btnDangNhap_Click(object sender, EventArgs e)
         {
-            MessageBox.Show(cbQuyen.SelectedIndex.ToString());
-        }
+            string id = txtMaNV.Text;
+            string passwd = txtMatKhau.Text;
+            NhanVien user = new NhanVien();
+            user.MaNV = id;
+            user.MatKhau = passwd;
+            switch(cbQuyen.SelectedIndex)
+            {
+                //TH người đăng nhập là: 
+                //Quản lý
+                case 0: user.MaNV = 8;
+                    break;
+                //CSR
+                case 1: user.MaNV = 5;
+                    break;
+                //Khác
+                case 2; user.MaNV = 0;
+                    break;
+            }
+
+            AuthenticationBUS bus = new AuthenticationBUS();
+            bus.verifyAccount(user);
     }
 }
